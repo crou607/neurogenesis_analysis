10955e7fc55709e180c1b6e4711ac2800f277a3e
crou607 <charalambos_rou@hotmail.com>
2026-06-09 14:35:10 +0100

    Update CrossCorrelationsOnCombinedData.m

--- CrossCorrelationsOnCombinedData.m
+++ CrossCorrelationsOnCombinedData.m
@@ -6,14 +6,9 @@
 close all
 clear all
 
-%% load isotropic data
+%% load data
 load("combined_data_for_correlationsNEW.mat")
-% for the last 18 columns dt = 30s, for all the rest dt = 25s
 
-% select which population to analyse: 1 for [1:end-18], 2 for
-% [end-18+1:end]
-
-% population = 1;
 maxLagMinutes = 23;
 analysis_number = 3;
 % 1 is NB area vs SC Intensity
